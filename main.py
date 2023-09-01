d428a976bf6da7535febb3056198593c7085a4d1
Chris Heise <crheise@icloud.com>
2023-09-01 12:06:28 -0600

    qa using a retriever

--- main.py
+++ main.py
@@ -2,86 +2,54 @@
 # Course: BSSD 4350 Agile Methodologies
 # Instructor: Jonathan Lee
 # Program: Langchain Research
-# Purpose: Try and manipulate the Langchain library
+# Purpose: Use the Langchain library to create grounding functionality.
 # File: main.py
 
-# Example code from
-# URL: https://python.langchain.com/docs/get_started/quickstart
-# URL: https://python.langchain.com/docs/modules/data_connection/retrievers/
-# URL: https://python.langchain.com/docs/modules/data_connection/document_loaders/pdf
-# URL: https://python.langchain.com/docs/modules/model_io/prompts/prompt_templates/
-# URL: https://python.langchain.com/docs/modules/chains/
-# URL: 
-# Date Accessed: 30 Aug 2023
+# Example code for QA using a Retriever
+# URL: https://python.langchain.com/docs/use_cases/question_answering/how_to/vector_db_qa
+# Date Accessed: 1 Sept 2023
 
+from environs import Env
+env = Env()
+env.read_env()
 
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.prompts import ChatPromptTemplate
-from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI 
 from langchain.vectorstores import Chroma
-from langchain.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
 
-# llm = OpenAI()
-llm = ChatOpenAI()
 
+# Load and process the source document (single PDF for now)
+# - There are other document loaders available (e.g. HTML, text, etc.)
 loader = PyPDFLoader("https://thediversitymovement.com/wp-content/uploads/2020/11/WW-SayThis-whitepaper_201116-F.pdf")
+documents = loader.load_and_split()
 
-# Below splits the document into pages and makes sure the document is loaded/split correctly
-# pages = loader.load_and_split()
-# print(pages[0])
+# Create embeddings with the source documents
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(documents, embeddings)
 
-index = VectorstoreIndexCreator().from_loaders([loader])
+# Create a custom prompt to give instruction
+# - We can customize our prompt to get the best formats for our use case
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know. Don't try to make up the answer.
+{context}
 
-#query = "What should I say instead of transgendered"
-#print(index.query(query))
-# >> 'Say "Tony is a transgender person," or "The parade included many transgender people."
+Question: {question}
+Answer:"""
 
-#query = "What should I say instead of addicted"
-#print(index.query(query))
-# >> 'A fan of or excellent or delicious'
-# The answer above is from a table, 'a fan of' is the replacement for 'addicted'
-#   'Excellent' and 'delicious' are the replacements for 'like crack'
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-template = ChatPromptTemplate.from_messages(
-    [
-        SystemMessage(
-            content=(
-                "You are a helpful assistant that re-writes the user's text to be more inclusive."
-            )
-        ),
-        HumanMessagePromptTemplate.from_template("{text}"),
-    ]
-)
+chain_type_kwargs = {"prompt": PROMPT}
 
-# This works, but it's not using any outside material (like the pdf) yet
-query = "I think Mary is a transgendered person"
-print(llm(template.format_messages(text=query)))
-# >> 'I think Mary is a transgender person.'
+# Create the QA retriever chain
+# - We can use other llms, different chain types/retrievers, and whatever our custom prompt is
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs, return_source_documents=True)
 
-# TODO: figure out how to combine using the template with the index (Chains? Data Augmented Generation?)
-#   Check: https://python.langchain.com/docs/use_cases/question_answering/how_to/vector_db_text_generation
-#   Check: https://python.langchain.com/docs/use_cases/question_answering/how_to/analyze_document
-#   Check: https://python.langchain.com/docs/use_cases/question_answering/how_to/multi_retrieval_qa_router
-#   Check: https://python.langchain.com/docs/modules/agents/how_to/custom_agent_with_tool_retrieval
-### Check: https://python.langchain.com/docs/use_cases/question_answering.html
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-data = loader.load_and_split()
-#all_splits = text_splitter.split_documents(data)
-#vectorstore = Chroma.from_documents(documents=data, embedding=OpenAIEmbeddings())
-
-#question = "What should I say instead of crazy?"
-#docs = vectorstore.similarity_search(question)
-
-# The above results in errors. I don't think I'm combining things correctly yet
-
-# This won't work yet. Need to pip install faiss
-faiss_index = FAISS.from_documents(documents=data, embedding=OpenAIEmbeddings())
-docs = faiss_index.similarity_search("What should I say instead of crazy?")
-for doc in docs:
-    print(str(doc.metadata["page"]) + ": " + doc.page_content[:100])
+# Ask a question, show the answer, and show the sources
+query = "What should I say instead of transgendered?"
+result = qa({"query": query})
+print(result["result"])
+# >> "Transgender person or transgender people."
+print(result["source_documents"])
+# >> Returns the document (page) that the answer was found on as well as its metadata
