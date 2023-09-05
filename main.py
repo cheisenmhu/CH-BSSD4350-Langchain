f128fe4f470fe8956edc1f0341456765ee35a8f2
Chris Heise <crheise@icloud.com>
2023-09-05 17:09:34 -0600

    expanded prompt

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ from langchain.prompts import PromptTemplate
 # Load and process the source document (single PDF for now)
 # - There are other document loaders available (e.g. HTML, text, etc.)
 loader = PyPDFLoader("https://thediversitymovement.com/wp-content/uploads/2020/11/WW-SayThis-whitepaper_201116-F.pdf")
-documents = loader.load_and_split()     # Returns a list of documents
+documents = loader.load_and_split()     # Splits with Recursive Character Text Splitter and returns a list of documents
+#print(documents[1])
 
 # Create vector store from the source documents
 embeddings = OpenAIEmbeddings()        # This uses the OpenAI API Key from the environment
@@ -33,11 +34,16 @@ docsearch = Chroma.from_documents(documents, embeddings)    # Creates a vector s
 
 # Create a custom prompt to give instruction
 # - We can customize our prompt to get the best formats for our use case
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say you don't know. Don't try to make the answer up.
+# - More on custom prompts: https://stackoverflow.com/questions/76697681/retrieval-qa-with-custom-prompt-with-multiple-inputs-and-memory
+prompt_template = """You are a helpful text-editing assistant.
+Use the following pieces of context to re-write the query to be more inclusive.
+You should re-write text to be more inclusive by avoiding gendered language, ableist language, and other forms of discriminatory language.
+Don't make things up; only refer to the context. Don't offer any reasoning, only re-write the text.
 {context}
 
 Question: {question}
-Answer:"""
+Answer:
+"""
 
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
@@ -48,13 +54,13 @@ chain_type_kwargs = {"prompt": PROMPT}
 qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs, return_source_documents=True)   # Also uses the API key
 
 # Ask a question, show the answer, and show the sources
-text = "I think Mary is transgenered."
+text = "Are you a boy or a girl? Do you go by 'he' or 'she'?"
 query = f"How should the following be rewritten to be more inclusive? {text}"
 
 result = qa({"query": query})
 
 print(result["result"])
-# >> "I think Mary is transgender." (still isn't perfect, because you shouldn't gossip about someone's gender identity. But it's progress.)
+# >> "What pronouns do you use?"
 
-print(result["source_documents"])
+#print(result["source_documents"])
 # >> Returns the document that the answer was found on as well as its metadata
